c1a103407fdc7943fdf4186256ca90afdd54a846
Aleksandr Khasanov <sashokhasanov@mail.ru>
2022-03-24 12:08:53 +0300

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Сашок on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
