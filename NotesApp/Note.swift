210acaf36be5d64bc40f5f20a870702da04aee79
Aleksandr Khasanov <sashokhasanov@mail.ru>
2022-03-27 19:56:26 +0300

    minor fix

--- NotesApp/Note.swift
+++ NotesApp/Note.swift
@@ -16,10 +16,10 @@ struct Note {
     
     static func getMockData() -> [Note] {
         [
-            Note(id: UUID(), title: "Первая заметка", content: "Тест тест тест тест тест", color: 0xfefefefe, date: Date()),
+            Note(id: UUID(), title: "Первая заметка", content: "Тест тест тест тест тест", color: 0xffff00ff, date: Date()),
             Note(id: UUID(), title: "Вторая заметка", content: "Тест тест тест тест тест", color: 0xff00ff00, date: Date()),
             Note(id: UUID(), title: "Третья заметка", content: "Тест тест тест тест тест", color: 0xffff0000, date: Date()),
-            Note(id: UUID(), title: "Четвертая заметка", content: "Тест тест тест тест тест", color: 0x0f0000ff, date: Date())
+            Note(id: UUID(), title: "Четвертая заметка", content: "Тест тест тест тест тест", color: 0xff0000ff, date: Date())
         ]
     }
 }
