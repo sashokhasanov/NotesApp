099c2b005c0cf55d779ddd728dc800b74dbe6bcc
Aleksandr Khasanov <sashokhasanov@mail.ru>
2022-03-27 17:32:53 +0300

    model draft + setting model data to controls

---
+++ NotesApp/Note.swift
@@ -0,0 +1,16 @@
+//
+//  Note.swift
+//  NotesApp
+//
+//  Created by Сашок on 27.03.2022.
+//
+
+import Foundation
+
+struct Note {
+    let id: UUID
+    let title: String
+    let content: String
+    let color: Int32
+    let date: Date
+}
--- NotesApp/NoteViewController.swift
+++ NotesApp/NoteViewController.swift
@@ -16,6 +16,9 @@ class NoteViewController: UIViewController {
     @IBOutlet var colorViews: [CircleMarkerView]!
     @IBOutlet weak var customColorView: GradientMarkerView!
     
+    // MARK: - Internal properties
+    var note: Note?
+    
     // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +27,11 @@ class NoteViewController: UIViewController {
     
     // MARK: - IBActions
     @IBAction func predefinedColorTapped(_ sender: UITapGestureRecognizer) {
-        guard let currentView = sender.view else {
+        guard let currentView = sender.view as? CircleMarkerView else {
             return
         }
         
-        for view in colorViews {
-            view.showMarker = view === currentView as? CircleMarkerView
-        }
+        selectColorView(colorView: currentView)
         
         if let color = currentView.backgroundColor {
             updateNoteMarker(with: color)
@@ -46,6 +47,12 @@ class NoteViewController: UIViewController {
         showColorPicker(with: customColorView.backgroundColor)
     }
     
+    private func selectColorView(colorView: CircleMarkerView) {
+        for view in colorViews {
+            view.showMarker = view === colorView
+        }
+    }
+    
     private func updateNoteMarker(with color: UIColor) {
         UIView.animate(withDuration: 0.3) {
             self.noteMarkerView.backgroundColor = color
@@ -59,6 +66,58 @@ class NoteViewController: UIViewController {
         }
         colorPickerController.delegate = self
         present(colorPickerController, animated: true)
+    }
+    
+    private func setDataToControls() {
+        guard let note = note else { return }
+        
+        noteTitleTextField.text = note.title
+        noteContentTextView.text = note.content
+        setupNoteColor()
+    }
+    
+    private func setupNoteColor() {
+        guard let note = note else { return }
+        
+        for colorView in colorViews {
+            if colorView is GradientMarkerView {
+                customColorView.backgroundColor = getColor(from: note.color)
+            }
+
+            if checkNeedSelectColorView(view: colorView) {
+                selectColorView(colorView: colorView)
+                if let color = colorView.backgroundColor {
+                    updateNoteMarker(with: color)
+                }
+            }
+        }
+    }
+    
+    private func checkNeedSelectColorView(view: CircleMarkerView) -> Bool {
+        guard !(view is GradientMarkerView) else { return true }
+        guard let color = view.backgroundColor, let note = note else { return false }
+        
+        return note.color == getColorValue(from: color)
+    }
+    
+    private func getColor(from value: Int32) -> UIColor {
+        let alpha = CGFloat((value >> 24) & 0xFF) / 255.0
+        let red = CGFloat((value >> 16) & 0xFF) / 255.0
+        let green = CGFloat((value >> 8) & 0xFF) / 255.0
+        let blue = CGFloat(value & 0xFF) / 255.0
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
+    private func getColorValue(from color: UIColor) -> Int32 {
+        let currentColor = color.ciColor
+        
+        let red = Int32(currentColor.red * 255.0)
+        let green = Int32(currentColor.green * 255.0)
+        let blue = Int32(currentColor.blue * 255.0)
+        let alpha = Int32(currentColor.alpha * 255.0)
+        
+        return Int32((alpha << 24) + (red << 16) + (green << 8) + blue)
     }
 }
 
@@ -102,7 +161,7 @@ extension NoteViewController: UITextFieldDelegate {
 // MARK: Color picker view controller delegate
 extension NoteViewController: UIColorPickerViewControllerDelegate {
     func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
-        updateNoteMarker(with: viewController.selectedColor)
         customColorView.backgroundColor = viewController.selectedColor
+        updateNoteMarker(with: viewController.selectedColor)
     }
 }
