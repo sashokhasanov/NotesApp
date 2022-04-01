ffc53946c280de5a36e143608b242b195d549e94
Aleksandr Khasanov <sashokhasanov@mail.ru>
2022-04-01 17:50:37 +0300

    keychain service v.2

--- NotesApp/KeyChainService.swift
+++ NotesApp/KeyChainService.swift
@@ -9,94 +9,49 @@ import Foundation
 
 class KeyChainService {
     static let shared = KeyChainService()
-    private let serviceId = "ru.awesome.NotesApp.KeychainService"
+    private let serviceId = Bundle.main.bundleIdentifier ?? "ru.awesome.NotesApp.KeychainService"
     
     private init() {}
     
-    func set(value: Data, account: String) throws {
+    @discardableResult func set(value: Data, account: String) -> Bool {
 
-        if try exists(account: account) {
-            try update(value: value, account: account)
-        } else {
-            try add(value: value, account: account)
-        }
-    }
-    
-    func get(account: String) throws -> Data? {
-        var result: AnyObject?
-
-        let status = SecItemCopyMatching([kSecClass: kSecClassGenericPassword,
-                                          kSecAttrService: serviceId,
-                                          kSecAttrAccount: account,
-                                          kSecReturnData: true] as NSDictionary, &result)
-
-        switch status {
-        case errSecSuccess:
-            return result as? Data
-        case errSecItemNotFound:
-            return nil
-        default:
-            throw KeyChainError.readError
-        }
-    }
-    
-    func delete(account: String) throws {
-        let status = SecItemDelete([kSecClass: kSecClassGenericPassword,
-                                    kSecAttrService: serviceId,
-                                    kSecAttrAccount: account] as NSDictionary)
-        
-        guard status == errSecSuccess else {
-            throw KeyChainError.deleteError
-        }
-    }
-
-    
-    private func exists(account: String) throws -> Bool {
-        let status = SecItemCopyMatching([kSecClass: kSecClassGenericPassword,
-                                          kSecAttrService: serviceId,
-                                          kSecAttrAccount: account,
-                                          kSecReturnData: false] as NSDictionary, nil)
-        
-        switch status {
-        case errSecSuccess:
-            return true
-        case errSecItemNotFound:
-            return false
-        default:
-            throw KeyChainError.lookUpError
-        }
-    }
-    
-    private func add(value: Data, account: String) throws {
         let status = SecItemAdd([kSecClass: kSecClassGenericPassword,
                                  kSecAttrService: serviceId,
                                  kSecAttrAccount: account,
                                  kSecAttrAccessible: kSecAttrAccessibleAfterFirstUnlock,
                                  kSecValueData: value] as NSDictionary, nil)
         
-        guard status == errSecSuccess else {
-            throw KeyChainError.createError
+        if status == errSecDuplicateItem {
+            return update(value: value, account: account)
         }
+        
+        return status == errSecSuccess
     }
     
-    private func update(value: Data, account: String) throws {
+    @discardableResult func get(account: String) -> Data? {
+        var result: AnyObject?
+
+        SecItemCopyMatching([kSecClass: kSecClassGenericPassword,
+                             kSecAttrService: serviceId,
+                             kSecAttrAccount: account,
+                             kSecReturnData: true] as NSDictionary, &result)
+        
+        return result as? Data
+    }
+    
+    @discardableResult func delete(account: String) -> Bool {
+        let status = SecItemDelete([kSecClass: kSecClassGenericPassword,
+                                    kSecAttrService: serviceId,
+                                    kSecAttrAccount: account] as NSDictionary)
+        
+        return status == errSecSuccess
+    }
+
+    private func update(value: Data, account: String) -> Bool {
         let status = SecItemUpdate([kSecClass: kSecClassGenericPassword,
                                     kSecAttrService: serviceId,
                                     kSecAttrAccount: account] as NSDictionary, [kSecValueData: value] as NSDictionary)
         
-        guard status == errSecSuccess else {
-            throw KeyChainError.updateError
-        }
-    }
-}
-
-extension KeyChainService {
-    enum KeyChainError: Error {
-        case createError
-        case lookUpError
-        case readError
-        case updateError
-        case deleteError
-        case unknownError
+        return status == errSecSuccess
     }
 }
